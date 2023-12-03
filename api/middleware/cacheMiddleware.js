b16a30368a924c9c91820e3b2f8038aa4339ada8
Ian Rocks <ian@ian.rocks>
2023-12-03 15:29:10 -0700

    Added the option to use a REDIS URL, or REDIS_HOST

--- api/middleware/cacheMiddleware.js
+++ api/middleware/cacheMiddleware.js
@@ -2,8 +2,12 @@ import Redis from 'ioredis';
 
 // Configure and create a Redis client
 const redisConfig = process.env.REDIS_URL
-  ? { url: process.env.REDIS_URL }
-  : { host: 'localhost', port: 6379 };
+    ? { url: process.env.REDIS_URL }
+    : {
+        host: process.env.REDIS_HOST || 'localhost',
+        port: process.env.REDIS_PORT || 6379
+    };
+    
 export const redis = new Redis(redisConfig);
 
 redis.on('error', (err) => console.error('Redis error:', err));
