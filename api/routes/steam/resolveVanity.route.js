47f4ec3b6796b1a19761209582971e7b7584d8d8
Ian Rocks <ian@ian.rocks>
2023-12-06 23:36:49 +0000

    Added new resolveVanity and custom pages

---
+++ api/routes/steam/resolveVanity.route.js
@@ -0,0 +1,26 @@
+import express from 'express';
+const router = express.Router();
+
+import { cacheMiddleware } from '../../middleware/cacheMiddleware.js';
+import { fetchSteamData } from '../../helpers/fetchSteamData.js';
+
+router.get('/resolveVanity/:vanityUrl', cacheMiddleware(1200), async (req, res) => {
+    const vanityUrl = req.params.vanityUrl;
+    const endpoint = '/ISteamUser/ResolveVanityURL/v0001/';
+    const queryParams = `vanityurl=${vanityUrl}`;
+
+    try {
+        const data = await fetchSteamData(endpoint, queryParams);
+        const steamId = data.response.steamid;
+
+        if (steamId) {
+            res.json({ steamId: steamId });
+        } else {
+            res.status(404).json({ error: 'Vanity URL not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Error resolving vanity URL' });
+    }
+});
+
+export default router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,8 @@
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
+
+import axios from 'axios';
+
 import SteamStatus from './components/steamStatus';
 import SteamRecentlyPlayedGames from './components/steamRecentlyPlayed';
 import SteamOwnedGames from './components/steamOwnedGames';
@@ -8,11 +13,44 @@ import SteamTotalPlaytimeCard from './components/steamTotalPlaytimeCard';
 import './App.css';
 
 const HomePage = () => {
-    const steamId = import.meta.env.VITE_APP_STEAM_ID;
+    const { steamName } = useParams();
+    const defaultSteamId = import.meta.env.VITE_APP_STEAM_ID;
+    const [steamId, setSteamId] = useState(defaultSteamId);
+    const [isLoading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchSteamId = async () => {
+            if (!steamName) return;
+
+            // Check if steamName is already a numeric Steam ID
+            if (/^\d+$/.test(steamName)) return setSteamId(steamName);
+
+            try {
+                setLoading(true);
+                const response = await axios.get(import.meta.env.VITE_APP_API_URL + `/resolveVanity/${steamName}`);
+                if (response.data?.steamId) {
+                    setSteamId(response.data.steamId);
+                } else {
+                    setError('Steam ID not found for given username');
+                }
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchSteamId();
+    }, [steamName]);
+
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
 
     return (
         <>
-            <h1>Welcome to My Steam Profile Page</h1>
+            <h1>Welcome to {steamName} Steam Profile Page</h1>
 
             <div className="steam-components">
                 <span><SteamAvatar steamId={steamId} /></span>
@@ -30,4 +68,15 @@ const HomePage = () => {
     );
 };
 
-export default HomePage;
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/:steamName" element={<HomePage />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
